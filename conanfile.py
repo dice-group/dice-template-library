11efe713009be85b597270d541f9b2a4fd6d941a
Lukas Kerkemeier <lukas.kerkemeier@web.de>
2022-03-17 15:45:05 +0100

    created the conanfile.py based on the hypertrie one.

---
+++ conanfile.py
@@ -0,0 +1,29 @@
+import os
+import re
+from conans import ConanFile, CMake
+from conans.tools import load
+
+
+class Hypertrie(ConanFile):
+    name = "DiceTemplateLibrary"
+    author = "DICE Group <info@dice-research.org>"
+    description = ""
+    homepage = "https://dice-research.org/"
+    url = homepage
+    settings = "build_type", "compiler", "os", "arch"
+    generators = "cmake", "cmake_find_package", "cmake_paths"
+    exports_sources = "include/*", "CMakeLists.txt", "cmake/*"
+    no_copy_source = True
+
+    def set_version(self):
+        if not hasattr(self, 'version') or self.version is None:
+            cmake_file = load(os.path.join(self.recipe_folder, "CMakeLists.txt"))
+            self.version = re.search(r"project\(\w+\s+VERSION\s+(\d+\.\d+)[^)]*\)", cmake_file).group(1)
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.install()
+
+    def package_id(self):
+        self.info.header_only()
