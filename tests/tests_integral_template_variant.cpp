4b50c8b54449e5f8b873088431ef7f288001d1c6
Alexander Bigerl <alexander@bigerl.eu>
2022-11-21 12:38:55 +0100

    renamed integral_templated_variant to integral_template_variant

--- tests/tests_integral_template_variant.cpp
+++ tests/tests_integral_template_variant.cpp
@@ -1,6 +1,6 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 
-#include <dice/template-library/integral_templated_variant.hpp>
+#include <dice/template-library/integral_template_variant.hpp>
 
 #include <doctest/doctest.h>
 
@@ -37,11 +37,11 @@ namespace dice::template_library {
 		CHECK(call_count == 1);
 	}
 
-	TEST_SUITE("integral_templated_variant") {
+	TEST_SUITE("integral_template_variant") {
 		TEST_CASE("asc pos") {
-			integral_templated_variant<Data, 2, 7> itv{Data<5>{}};
-			integral_templated_variant<Data, 2, 7> lower{Data<2>{}};
-			integral_templated_variant<Data, 2, 7> upper{Data<7>{}};
+			integral_template_variant<Data, 2, 7> itv{Data<5>{}};
+			integral_template_variant<Data, 2, 7> lower{Data<2>{}};
+			integral_template_variant<Data, 2, 7> upper{Data<7>{}};
 
 			CHECK(get<5>(itv) == 5);
 			CHECK(get<2>(lower) == 2);
@@ -61,9 +61,9 @@ namespace dice::template_library {
 		}
 
 		TEST_CASE("desc pos") {
-			integral_templated_variant<Data, 5, 1> itv{Data<3>{}};
-			integral_templated_variant<Data, 5, 1> lower{Data<5>{}};
-			integral_templated_variant<Data, 5, 1> upper{Data<1>{}};
+			integral_template_variant<Data, 5, 1> itv{Data<3>{}};
+			integral_template_variant<Data, 5, 1> lower{Data<5>{}};
+			integral_template_variant<Data, 5, 1> upper{Data<1>{}};
 
 			CHECK(get<3>(itv) == 3);
 			CHECK(get<5>(lower) == 5);
@@ -83,9 +83,9 @@ namespace dice::template_library {
 		}
 
 		TEST_CASE("asc neg") {
-			integral_templated_variant<Data, -6, -1> itv{Data<-3>{}};
-			integral_templated_variant<Data, -6, -1> lower{Data<-6>{}};
-			integral_templated_variant<Data, -6, -1> upper{Data<-1>{}};
+			integral_template_variant<Data, -6, -1> itv{Data<-3>{}};
+			integral_template_variant<Data, -6, -1> lower{Data<-6>{}};
+			integral_template_variant<Data, -6, -1> upper{Data<-1>{}};
 
 			CHECK(get<-3>(itv) == -3);
 			CHECK(get<-6>(lower) == -6);
@@ -105,9 +105,9 @@ namespace dice::template_library {
 		}
 
 		TEST_CASE("desc pos") {
-			integral_templated_variant<Data, -2, -8> itv{Data<-5>{}};
-			integral_templated_variant<Data, -2, -8> lower{Data<-2>{}};
-			integral_templated_variant<Data, -2, -8> upper{Data<-8>{}};
+			integral_template_variant<Data, -2, -8> itv{Data<-5>{}};
+			integral_template_variant<Data, -2, -8> lower{Data<-2>{}};
+			integral_template_variant<Data, -2, -8> upper{Data<-8>{}};
 
 			CHECK(get<-5>(itv) == -5);
 			CHECK(get<-2>(lower) == -2);
@@ -127,23 +127,23 @@ namespace dice::template_library {
 		}
 
 		TEST_CASE("single entry") {
-			integral_templated_variant<Data, 0, 0> itv{Data<0>{}};
+			integral_template_variant<Data, 0, 0> itv{Data<0>{}};
 			CHECK(get<0>(itv) == 0);
 		}
 
 		TEST_CASE("in place construction") {
-			integral_templated_variant<CompoundData, 0, 2> itv{std::in_place_type<CompoundData<1>>, 2.0, 3.f, 5};
+			integral_template_variant<CompoundData, 0, 2> itv{std::in_place_type<CompoundData<1>>, 2.0, 3.f, 5};
 			CHECK(get<1>(itv) == 1 + 2 + 3 + 5);
 		}
 
 		TEST_CASE("visit forwarding") {
-			integral_templated_variant<Data, 1, 1> const copyable{std::in_place_type<Data<1>>};
+			integral_template_variant<Data, 1, 1> const copyable{std::in_place_type<Data<1>>};
 
 			visit([](auto &&x) {
 				call_counted(std::forward<decltype(x)>(x));
 			}, copyable);
 
-			integral_templated_variant<Data, 1, 1> movable{std::in_place_type<Data<1>>};
+			integral_template_variant<Data, 1, 1> movable{std::in_place_type<Data<1>>};
 
 			visit([](auto &&x) {
 				call_counted(std::forward<decltype(x)>(x));
