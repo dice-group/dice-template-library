0909249145f59d75fe5037e3a1ba5bfe9d99c8f2
Liss Heidrich <Clueliss@users.noreply.github.com>
2023-03-06 14:09:01 +0100

    fix conan test package

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -3,7 +3,7 @@
 int main() {
 	int i = 7;
 	auto res = dice::template_library::switch_cases<5, 20>(
-			i, [](auto i) -> int { return i * 2; },
+			i, []<auto i>() -> int { return i * 2; },
 			[]() -> int { return -1; });
 	if (res != 14)
 		exit(1);
