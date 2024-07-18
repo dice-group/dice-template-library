d17ea1e3e5d06be1110c432bfa17bd561ad6b6d0
Liss Heidrich <31625940+Clueliss@users.noreply.github.com>
2024-07-18 09:53:52 +0200

    make clang-16 happy

--- examples/example_generator.cpp
+++ examples/example_generator.cpp
@@ -28,13 +28,13 @@ int main() {
 	//    D
 	//  B    F
 	// A C  E G
-	Tree leaf1{'A'};
-	Tree leaf2{'C'};
-	Tree leaf3{'E'};
-	Tree leaf4{'G'};
-	Tree branch1{'B', &leaf1, &leaf2};
-	Tree branch2{'F', &leaf3, &leaf4};
-	Tree root{'D', &branch1, &branch2};
+	Tree<char> leaf1{'A'};
+	Tree<char> leaf2{'C'};
+	Tree<char> leaf3{'E'};
+	Tree<char> leaf4{'G'};
+	Tree<char> branch1{'B', &leaf1, &leaf2};
+	Tree<char> branch2{'F', &leaf3, &leaf4};
+	Tree<char> root{'D', &branch1, &branch2};
 
 	std::string output;
 	for (char const x : root.traverse_inorder()) {
