3bcf4a3710e0196b2558e9bfb97c28adfa58f8e8
Lauri Vasama <git@vasama.org>
2026-05-17 10:32:28 +0300

    Add pointer_tag_pair
    Based on P3125 (constexpr pointer tagging), likely to be approved for
    standardisation early in the C++29 cycle.
    
    Permission is hereby granted to the dice-template-library project to use
    and relicense this code as seen fit. Additionally any requirements for
    attribution are waived.

---
+++ tests/tests_pointer_tag_pair.cpp
@@ -0,0 +1,21 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include <doctest/doctest.h>
+
+#include <dice/template-library/pointer_tag_pair.hpp>
+
+TEST_SUITE("pointer_tag_pair") {
+  using namespace dice::template_library;
+
+  struct alignas(4) s {};
+
+  using pair_type = pointer_tag_pair<s, unsigned>;
+  static_assert(pair_type::bits_requested == 2);
+
+  static s array[2];
+
+  TEST_CASE("constructor") {
+    auto const [p, t] = pair_type(array, 3);
+    REQUIRE(p == array);
+    REQUIRE(t == 3);
+  }
+}
