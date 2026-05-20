35f7bfa4e98b5bedca181a3ae1fc24b2973d54ca
Alex <alexander@tentris.io>
2026-05-20 10:27:04 +0200

    format test

--- tests/tests_pointer_tag_pair.cpp
+++ tests/tests_pointer_tag_pair.cpp
@@ -4,18 +4,19 @@
 #include <dice/template-library/pointer_tag_pair.hpp>
 
 TEST_SUITE("pointer_tag_pair") {
-  using namespace dice::template_library;
+    using namespace dice::template_library;
 
-  struct alignas(4) s {};
+    struct alignas(4) s {
+    };
 
-  using pair_type = pointer_tag_pair<s, unsigned>;
-  static_assert(pair_type::bits_requested == 2);
+    using pair_type = pointer_tag_pair<s, unsigned>;
+    static_assert(pair_type::bits_requested == 2);
 
-  static s array[2];
+    static s array[2];
 
-  TEST_CASE("constructor") {
-    auto const [p, t] = pair_type(array, 3);
-    REQUIRE(p == array);
-    REQUIRE(t == 3);
-  }
+    TEST_CASE("constructor") {
+        auto const [p, t] = pair_type(array, 3);
+        REQUIRE(p == array);
+        REQUIRE(t == 3);
+    }
 }
