da5d1c0c5ae3180a75ffab9811769bb75b9219e1
Liss Heidrich <31625940+liss-h@users.noreply.github.com>
2026-07-08 16:25:28 +0200

    implement DICE_SANDBOX

---
+++ include/dice/template-library/sandbox.hpp
@@ -0,0 +1,153 @@
+#ifndef DICE_TEMPLATELIBRARY_ASSERTABORTED_HPP
+#define DICE_TEMPLATELIBRARY_ASSERTABORTED_HPP
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include <functional>
+#include <iostream>
+#include <system_error>
+
+namespace dice::template_library {
+
+    /**
+     * Execution result of a subprocess
+     */
+    enum struct SubProcessResult : int {
+        ExitSuccess = -1,  ///< normal exit, code == 0
+        ExitFailure = -2,  ///< normal exit, code != 0
+        Interrupted = SIGINT,
+        IllegalInstruction = SIGILL,
+        Aborted = SIGABRT,
+        FloatingPointException = SIGFPE,
+        SegmentationFault = SIGSEGV,
+        Terminated = SIGTERM,
+    };
+
+    namespace detail_sandbox {
+        inline void clear_all_signal_handlers() {
+            static constexpr int max_sig = [] {
+#ifdef NSIG
+                return NSIG;
+#elifdef _NSIG
+                return _NSIG;
+#else
+                return 64; // Fallback
+#endif
+            }();
+
+            struct sigaction sa{};
+            sa.sa_handler = SIG_DFL;
+            sigemptyset(&sa.sa_mask);
+            sa.sa_flags = 0;
+
+            for (int i = 1; i < max_sig; ++i) {
+                ::sigaction(i, &sa, nullptr); // ignore errors (like EINVAL for unknown signals)
+            }
+        }
+
+        inline void flush_all_streams() {
+            // normally the fflush below also handles
+            // these but if std::ios_base::sync_with_stdio(false);
+            // was ever called they have their own buffers
+            std::cout.flush();
+            std::cerr.flush();
+
+            fflush(nullptr);
+
+            // OS file descriptors do not need to be flushed
+        }
+
+        template<typename F>
+            requires (std::is_invocable_r_v<void, F> || std::is_invocable_r_v<int, F>)
+        [[nodiscard]] int invoke_like_main(F &&func) noexcept {
+            if constexpr (std::is_same_v<std::invoke_result_t<F>, void>) {
+                std::invoke(std::forward<F>(func));
+                return 0;
+            } else {
+                return std::invoke(std::forward<F>(func));
+            }
+        }
+
+        struct SandBox {};
+
+        template<typename F>
+        [[nodiscard]] SubProcessResult operator+(SandBox, F func) {
+            static_assert(std::is_invocable_r_v<void, F> || std::is_invocable_r_v<int, F>,
+                          "Function must be invocable like a main() function");
+
+            int const pid = fork();
+            if (pid < 0) {
+                throw std::system_error{errno, std::system_category(), "Unable to fork"};
+            }
+
+            if (pid == 0) {
+                // child process
+
+                // flush all buffered C file streams (including stdout)
+                fflush(nullptr);
+
+                // remove signal handles that may have been installed (e.g. by doctest)
+                clear_all_signal_handlers();
+
+                // ensure no stale data is in output streams
+                flush_all_streams();
+
+                // invoke user provided function
+
+                int const exit_code = invoke_like_main(std::move(func));
+
+                // ensure no destructors can run even if func does not abort
+                _exit(exit_code);
+            }
+
+            // parent process
+
+            int wstatus;
+            int res;
+            do {
+                res = ::waitpid(pid, &wstatus, 0);
+            } while (res < 0 && errno == EINTR);
+
+            if (res < 0) {
+                throw std::system_error{errno, std::system_category(), "waitpid failed"};
+            }
+
+            if (WIFEXITED(wstatus)) {
+                if (WEXITSTATUS(wstatus) == 0) {
+                    return SubProcessResult::ExitSuccess;
+                } else {
+                    return SubProcessResult::ExitFailure;
+                }
+            } else if (WIFSIGNALED(wstatus)) {
+                return static_cast<SubProcessResult>(WTERMSIG(wstatus));
+            } else {
+                // this should be unreachable with the flags passed to waitpid
+                throw std::runtime_error{"Process exited in a weird way"};
+            }
+        }
+    } // namespace detail_sandbox
+} // namespace dice::template_library
+
+/**
+ * Run the provided code in a separate process.
+ * This can, for example, be used to ensure that a particular piece of code triggers an assertion.
+ *
+ * @pre The application is single threaded when this is called,
+ *      or at least the other threads are not holding any global locks.
+ *
+ * @example
+ * @code
+ * auto const res = DICE_SANDBOX {
+ *     assert(false);
+ * };
+ *
+ * assert(res == SubProcessResult::Aborted);
+ * @endcode
+ */
+#define DICE_SANDBOX ::dice::template_library::detail_sandbox::SandBox{} + [=]() mutable
+
+#endif // DICE_TEMPLATELIBRARY_ASSERTABORTED_HPP
---
+++ tests/tests_sandbox.cpp
@@ -0,0 +1,191 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "dice/template-library/defer.hpp"
+
+#include <doctest/doctest.h>
+
+#include <dice/template-library/sandbox.hpp>
+
+#include <csignal>
+#include <cstdio>
+#include <fstream>
+#include <string>
+
+#include <fcntl.h>
+#include <unistd.h>
+
+TEST_SUITE("DICE_SANDBOX") {
+    using namespace dice::template_library;
+
+    TEST_CASE("success") {
+        SUBCASE("fallthrough") {
+            auto const res = DICE_SANDBOX {};
+
+            CHECK_EQ(res, SubProcessResult::ExitSuccess);
+        }
+        SUBCASE("explicit 0") {
+            auto const res = DICE_SANDBOX {
+                return 0;
+            };
+
+            CHECK_EQ(res, SubProcessResult::ExitSuccess);
+        }
+        SUBCASE("std::exit") {
+            auto const res = DICE_SANDBOX {
+                std::exit(0);
+            };
+
+            CHECK_EQ(res, SubProcessResult::ExitSuccess);
+        }
+    }
+
+    TEST_CASE("normal exit failure") {
+        SUBCASE("return") {
+            auto const res = DICE_SANDBOX {
+                return 42;
+            };
+
+            CHECK_EQ(res, SubProcessResult::ExitFailure);
+        }
+        SUBCASE("std::exit") {
+            auto const res = DICE_SANDBOX {
+                std::exit(12);
+            };
+
+            CHECK_EQ(res, SubProcessResult::ExitFailure);
+        }
+    }
+
+    TEST_CASE("exception") {
+        auto const res = DICE_SANDBOX {
+            throw std::runtime_error{"Hello"};
+        };
+
+        CHECK_EQ(res, SubProcessResult::Aborted);
+    }
+
+    TEST_CASE("std::abort") {
+        auto const res = DICE_SANDBOX {
+            std::abort();
+        };
+
+        CHECK_EQ(res, SubProcessResult::Aborted);
+    }
+
+    TEST_CASE("object sharing") {
+        std::vector<int> x{};
+
+        auto const res = DICE_SANDBOX {
+            x.push_back(12);
+        };
+
+        CHECK_EQ(res, SubProcessResult::ExitSuccess);
+        CHECK(x.empty());
+    }
+
+    TEST_CASE("stdout sharing") {
+        std::cout << "Hello "; // no flush
+
+        auto const res = DICE_SANDBOX {
+            std::cout << "World";
+        };
+
+        std::cout.flush();
+
+        CHECK_EQ(res, SubProcessResult::ExitSuccess);
+    }
+
+    TEST_CASE("stderr sharing") {
+        std::cerr << "Hello "; // no flush
+
+        auto const res = DICE_SANDBOX {
+            std::cerr << "World";
+        };
+
+        std::cerr.flush();
+
+        CHECK_EQ(res, SubProcessResult::ExitSuccess);
+    }
+
+    TEST_CASE("signal mapping") {
+        SUBCASE("SIGINT") {
+            auto const res = DICE_SANDBOX {
+                ::raise(SIGINT);
+            };
+            CHECK_EQ(res, SubProcessResult::Interrupted);
+        }
+        SUBCASE("SIGILL") {
+            auto const res = DICE_SANDBOX {
+                ::raise(SIGILL);
+            };
+            CHECK_EQ(res, SubProcessResult::IllegalInstruction);
+        }
+        SUBCASE("SIGFPE") {
+            auto const res = DICE_SANDBOX {
+                ::raise(SIGFPE);
+            };
+            CHECK_EQ(res, SubProcessResult::FloatingPointException);
+        }
+        SUBCASE("SIGSEGV") {
+            auto const res = DICE_SANDBOX {
+                ::raise(SIGSEGV);
+            };
+            CHECK_EQ(res, SubProcessResult::SegmentationFault);
+        }
+        SUBCASE("SIGTERM") {
+            auto const res = DICE_SANDBOX {
+                ::raise(SIGTERM);
+            };
+            CHECK_EQ(res, SubProcessResult::Terminated);
+        }
+        SUBCASE("SIGABRT") {
+            auto const res = DICE_SANDBOX {
+                ::raise(SIGABRT);
+            };
+            CHECK_EQ(res, SubProcessResult::Aborted);
+        }
+    }
+
+    TEST_CASE("real segfault") {
+        auto const res = DICE_SANDBOX {
+            // force real segfault
+            *static_cast<int volatile *>(nullptr) = 42;
+        };
+
+        CHECK_EQ(res, SubProcessResult::SegmentationFault);
+    }
+
+    TEST_CASE("value capture") {
+        int const y = 42;
+
+        auto const res = DICE_SANDBOX {
+            return y - 42; // uses the [=]-captured y
+        };
+
+        CHECK_EQ(res, SubProcessResult::ExitSuccess);
+    }
+
+    TEST_CASE("file descriptor side effects propagate") {
+        // in contrast to "object sharing": memory is copy-on-write, but effects
+        // on shared OS file descriptors are visible to the parent
+        char path[] = "/tmp/dice_sandbox_XXXXXX";
+        int const fd = ::mkstemp(path);
+        REQUIRE(fd >= 0);
+
+        DICE_DEFER {
+            ::close(fd);
+            ::unlink(path);
+        };
+
+        auto const res = DICE_SANDBOX {
+            ::write(fd, "hello", 5);
+        };
+
+        CHECK_EQ(res, SubProcessResult::ExitSuccess);
+
+        ::lseek(fd, 0, SEEK_SET);
+        char buf[6] = {};
+        auto const n = ::read(fd, buf, 5);
+        CHECK_EQ(n, 5);
+        CHECK_EQ(std::string_view{buf}, "hello");
+    }
+}
